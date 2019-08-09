05d41cc9597a8f091ea25488bc077d33c0197da0
SerezhaIvanov <serezha.ivanov02@gmail.com>
2019-08-09 13:58:52 +0200

    Update EventListener.php

--- src/SIvanov/EventListener.php
+++ src/SIvanov/EventListener.php
@@ -9,6 +9,7 @@ use pocketmine\block\BlockFactory;
 use pocketmine\block\Chest;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\Item;
 use pocketmine\level\particle\DestroyBlockParticle;
 use pocketmine\math\Vector3;
 
@@ -29,16 +30,28 @@ class EventListener implements Listener
 
         if ($block instanceof Chest) {
             $event->setCancelled(true);
-            $tile = $player->getLevel()->getTile($block->asVector3());
 
-            foreach ($tile->getInventory()->getContents() as $item) {
-                $player->getLevel()->setBlock($block->asVector3(), Block::get(0));
+            $player->getLevel()->setBlock($block->asVector3(), Block::get(0));
 
-                $player->getLevel()->dropItem($block->asVector3(), $item);
+            $weapon = array(256, 257, 258, 261, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279);
+            $items = array(320, 340, 360, 264, 265, 266, 332, 352);
+            $throwable = array(262, 332, 344);
+            $blocks = array(1, 4, 5, 46, 35, 45, 98, 24);
+            $armor = array(298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317);
 
-                $particle = new DestroyBlockParticle($block->asVector3(), BlockFactory::get(Block::CHEST));
-                $player->getLevel()->addParticle($particle);
-            }
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($weapon[array_rand($weapon)], 0, 1));
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($items[array_rand($items)], 0, mt_rand(1, 5)));
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($items[array_rand($items)], 0, mt_rand(1, 5)));
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($items[array_rand($items)], 0, mt_rand(1, 5)));
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($throwable[array_rand($throwable)], 0, mt_rand(8, 16)));
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($throwable[array_rand($throwable)], 0, mt_rand(8, 16)));
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($blocks[array_rand($blocks)], 0, mt_rand(16, 32)));
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($blocks[array_rand($blocks)], 0, mt_rand(16, 32)));
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($armor[array_rand($armor)], 0, 1));
+            $player->getLevel()->dropItem($block->asVector3(), Item::get($armor[array_rand($armor)], 0, 1));
+
+            $particle = new DestroyBlockParticle($block->asVector3(), BlockFactory::get(Block::CHEST));
+            $player->getLevel()->addParticle($particle);
         }
     }
 }
