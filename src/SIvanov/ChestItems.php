962124b7dac4ddd4a01d91e02783a880e7da89eb
SerezhaIvanov <serezha.ivanov02@gmail.com>
2019-08-09 13:54:43 +0200

    kek

--- src/SIvanov/ChestItems.php
+++ src/SIvanov/ChestItems.php
@@ -2,11 +2,14 @@
 
 namespace SIvanov;
 
+use pocketmine\plugin\PluginBase;
+
 Class ChestItems extends PluginBase
 {
 
     public function onEnable()
     {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->getLogger()->info("Автор: vk.com/serezhaivanov14");
     }
 }
---
+++ src/SIvanov/EventListener.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace SIvanov;
+
+
+use pocketmine\block\Block;
+use pocketmine\block\BlockFactory;
+use pocketmine\block\Chest;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\level\particle\DestroyBlockParticle;
+use pocketmine\math\Vector3;
+
+class EventListener implements Listener
+{
+
+    public $plugin;
+
+    public function __construct(ChestItems $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    public function onInteract(PlayerInteractEvent $event)
+    {
+        $player = $event->getPlayer();
+        $block = $event->getBlock();
+
+        if ($block instanceof Chest){
+            $event->setCancelled(true);
+            $tile = $player->getLevel()->getTile($block->asVector3());
+
+            if ($tile instanceof \pocketmine\tile\Chest){
+                foreach ($tile->getInventory()->getContents() as $item){
+                    $player->getLevel()->setBlock($block->asVector3(), Block::get(0));
+
+                    $player->getLevel()->dropItem($block->asVector3(), $item);
+
+                    $particle = new DestroyBlockParticle($block->asVector3(), BlockFactory::get(Block::CHEST));
+                    $player->getLevel()->addParticle($particle);
+                }
+            }
+        }
+    }
+
+}
